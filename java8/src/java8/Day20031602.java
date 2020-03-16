f1be2d1b2e2214ec9d15a6dbff17672aed0e6ca2
KimSiEon <kimsieon.dev@gmail.com>
2020-03-16 13:06:07 +0900

    3일차 변수의 범위

---
+++ java8/src/java8/Day20031602.java
@@ -0,0 +1,15 @@
+package java8;
+
+public class Day20031602 {
+	public static void main(String[] args) {
+		int number1 = 100; // 지역변수
+		System.out.println(number1);
+		{ // 중괄호블럭
+			System.out.println(number1);
+			int number2 = 200; // 블럭변수
+			System.out.println(number2);
+		}
+		System.out.println(number1);
+//		System.out.println(number2);
+	}
+}
