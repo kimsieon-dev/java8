0b3e22e8a01ed5ab5c8462561b4575a69080ff20
KimSiEon <kimsieon.dev@gmail.com>
2020-03-16 14:56:05 +0900

    3일차 대입연산자

---
+++ java8/src/java8/Day20301606.java
@@ -0,0 +1,26 @@
+package java8;
+
+public class Day20301606 {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 3;
+		
+		a += b; // a = a + b
+		System.out.println(a);
+		a -= b; // a = a - b
+		System.out.println(a);
+		a *= b; // a = a * b
+		System.out.println(a);
+		System.out.println("==========");
+		
+		a = 10;
+		System.out.println(a);
+		a *= 3 + 10; // a = a * 13
+		System.out.println(a);
+		a += 1; // a = 130 + 1
+		System.out.println(a);
+		a += 1; // a = 131 + 1
+		System.out.println(a);
+		
+	}
+}
