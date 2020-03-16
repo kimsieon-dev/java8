1ab26ad2097258b508dc174831bb7a1d6f3c0dfa
KimSiEon <kimsieon.dev@gmail.com>
2020-03-16 14:08:53 +0900

    3일차 관계연산자

--- java8/src/java8/Day20031604.java
+++ java8/src/java8/Day20031604.java
@@ -27,7 +27,7 @@ public class Day20031604 {
 		 * =을 1개 사용하면 대입연산자로 오른쪽의 값을 왼쪽으로 대입합니다.
 		 * ==을 2개 사용하면 왼쪽과 오른쪽 값이 동일한지를 판단합니다.
 		 */
-		
+		ff
 		boolean result = num1 < num2; // boolean -> 참, 거짓
 		System.out.println(result);
                                
