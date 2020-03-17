66bbcd9cb2456ee584310c9ace8c8a8ee660eb2f
KimSiEon <kimsieon.dev@gmail.com>
2020-03-17 15:08:53 +0900

    4일차 실습문제 3

---
+++ java8/src/java8/Day20031705.java
@@ -0,0 +1,16 @@
+package java8;
+
+public class Day20031705 {
+	public static void main(String[] args) {
+		int intyear = 1998;
+		int intmonth = 05;
+		int intdate = 27;
+		double dblweight = 63.2;
+		double dblheight = 176.5;
+		
+		System.out.println("积斥岿老 : " + intyear + "." + intmonth + "." + intdate);
+		System.out.println("个公霸 : " + dblweight);
+		System.out.println("虐 : " + dblheight);
+		
+	}
+}
