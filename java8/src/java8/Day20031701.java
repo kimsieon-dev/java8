bbae816db3625268bc738e2a354a6b44eae0c3aa
KimSiEon <kimsieon.dev@gmail.com>
2020-03-17 14:02:44 +0900

    4일차 System.in.read() 예제

---
+++ java8/src/java8/Day20031701.java
@@ -0,0 +1,18 @@
+package java8;
+
+import java.io.IOException;
+
+public class Day20031701 {
+	public static void main(String[] args) throws IOException {
+		/*
+		 * IOException : 입출력 예외
+		 * I -> Input
+		 * O -> Output
+		 * Exception -> 예외
+		 */
+		
+		System.out.println("키보드를 눌러서 키를 입력해 주세요.");
+		int a = System.in.read();
+		System.out.println(a);
+	}
+}
