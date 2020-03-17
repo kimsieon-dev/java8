280e2b5efd841038465d32e20c4e8424ed41642e
KimSiEon <kimsieon.dev@gmail.com>
2020-03-17 15:09:23 +0900

    4일차 연습문제 4

---
+++ java8/src/java8/Day20031706.java
@@ -0,0 +1,23 @@
+package java8;
+
+public class Day20031706 {
+	public static void main(String[] args) {
+		int a = 8;
+		int b = 5;
+		int res1 = a + b;
+		int res2 = a - b;
+		int res3 = a * b;
+		int res4 = a / b;
+		int res5 = a % b;
+		double res6 = (double)a / b;
+		
+		System.out.println("a + b = " + res1);
+		System.out.println("a - b = " + res2);
+		System.out.println("a * b = " + res3);
+		System.out.println("a / b = " + res4);
+		System.out.println("a % b = " + res5);
+		System.out.println("a / b = " + res6);
+		
+		
+	}
+}
