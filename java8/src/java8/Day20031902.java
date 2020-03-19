84d4cb7c08396f5e67e373e05a5f1e0c7c8267b0
KimSiEon <kimsieon.dev@gmail.com>
2020-03-19 14:46:30 +0900

    else if

--- java8/src/java8/Day20031902.java
+++ java8/src/java8/Day20031902.java
@@ -8,21 +8,21 @@ public class Day20031902 {
 		System.out.print("당신의 영어 점수를 입력해 주세요.(0~100) : ");
 		int score = sc.nextInt();
 		String pass = "";
-		
-		if(score >= 90) {
+
+		if (score >= 90) {
 			pass = "A학점";
-		}else if(score >= 80) {
+		} else if (score >= 80) {
 			pass = "B학점";
-		}else if(score >= 70) {
+		} else if (score >= 70) {
 			pass = "C학점";
-		}else if(score >= 60 ) {
+		} else if (score >= 60) {
 			pass = "D학점";
-		}else {
+		} else {
 			pass = "F학점";
 		}
-		
+
 		System.out.println("시험 성적은" + pass + "입니다.");
-		
+
 		sc.close();
 	}
 }
