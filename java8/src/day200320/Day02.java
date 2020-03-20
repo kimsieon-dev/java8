77de3fe39ce218b88254ec516ad746943ef65c1c
KimSiEon <kimsieon.dev@gmail.com>
2020-03-20 13:57:19 +0900

    반복문 for 3

---
+++ java8/src/day200320/Day02.java
@@ -0,0 +1,12 @@
+package day200320;
+
+public class Day02 {
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+		for (int i = 0; i < 5; i++) {
+			System.out.println("hello");
+		}
+	}
+}
