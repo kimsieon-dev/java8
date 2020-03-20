974f96afa7ec2efc96cdf8a19cf929792df9ba0f
KimSiEon <kimsieon.dev@gmail.com>
2020-03-20 15:46:37 +0900

    실습문제

--- java8/src/day200320/Day09.java
+++ java8/src/day200320/Day09.java
@@ -10,10 +10,10 @@ public class Day09 {
 		System.out.print("윤년을 판단할 년도 입력 : ");
 		year = sc.nextInt();
 
-		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+		if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
 			System.out.println(year + "년은 윤년이다.");
 		} else {
-			System.out.println(year + "년은 윤년이 아니다.");
+			System.out.println(year + "년은 윤년이 아니다."); 
 		}
 	}
 }
