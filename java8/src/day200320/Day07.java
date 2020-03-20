a21ac41cba1755ba1bdab865a28c548f2cae9502
KimSiEon <kimsieon.dev@gmail.com>
2020-03-20 14:57:29 +0900

    반복문 while 2

---
+++ java8/src/day200320/Day07.java
@@ -0,0 +1,13 @@
+package day200320;
+
+public class Day07 {
+	public static void main(String[] args) {
+		int i = 0;
+		
+		// 조건에 true를 입력하면 무한반복
+		while (true) {
+			System.out.println("Hello!");
+			i++;
+		}
+	}
+}
