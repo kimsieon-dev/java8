3b626869ffb11fde0c3feff4993c0bc11183e18b
KimSiEon <kimsieon.dev@gmail.com>
2020-03-20 14:53:08 +0900

    반복문 while

---
+++ java8/src/day200320/Day06.java
@@ -0,0 +1,16 @@
+package day200320;
+
+public class Day06 {
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+		System.out.println("==========");
+
+		int index = 0;
+		while (index < 10) {
+			System.out.println(index);
+			index++;
+		}
+	}
+}
