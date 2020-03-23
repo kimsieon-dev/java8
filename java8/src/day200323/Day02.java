77c896cc99f964d7c4bce2bf788d55879c8dd1ea
KimSiEon <kimsieon.dev@gmail.com>
2020-03-23 09:36:48 +0900

    for 문 연습문제

---
+++ java8/src/day200323/Day02.java
@@ -0,0 +1,13 @@
+package day200323;
+
+public class Day02 {
+	public static void main(String[] args) {
+		int i;
+		int sum = 0;
+
+		for (i = 1; i <= 10; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+	}
+}
