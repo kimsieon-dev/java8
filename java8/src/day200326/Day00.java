a5f79d74daa53af873e0b3ba251205b5a0a63c6c
KimSiEon <kimsieon.dev@gmail.com>
2020-03-26 09:14:29 +0900

    for 문 a ~ z 출력

---
+++ java8/src/day200326/Day00.java
@@ -0,0 +1,17 @@
+package day200326;
+
+public class Day00 {
+	public static void main(String[] args) {
+		int i;
+		for(i = 'a'; i <= 'z'; i++) {
+			System.out.print((char)i);
+		}
+		System.out.println();
+		System.out.println("==========");
+		
+		for(i ='A'; i <= 'Z'; i++) {
+			System.out.print((char)i);
+		}
+		System.out.println();
+	}
+}
