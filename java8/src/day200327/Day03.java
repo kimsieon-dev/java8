2ca81e6aef5db3c83ae330e1e6d7b850cb480d4c
KimSiEon <kimsieon.dev@gmail.com>
2020-03-27 10:38:56 +0900

    배열 연습문제4

---
+++ java8/src/day200327/Day03.java
@@ -0,0 +1,37 @@
+package day200327;
+
+public class Day03 {
+	public static void main(String[] args) {
+		int[][] arr = new int[5][7];
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + "\t");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		int cnt = 1;
+		for (int i = 0; i < arr.length; i++) {
+			if (i % 2 == 0) {
+				for (int j = 0; j < arr[i].length; j++) {
+					arr[i][j] = cnt;
+					cnt++;
+				}
+			} else {
+				for (int j = arr[i].length - 1; j >= 0; j--) {
+					arr[i][j] = cnt;
+					cnt++;
+				}
+			}
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
+}
