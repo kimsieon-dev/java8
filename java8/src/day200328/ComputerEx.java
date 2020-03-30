ed76937c2721f27c975fc6820f5c57f716f70acb
KimSiEon <kimsieon.dev@gmail.com>
2020-03-30 12:51:29 +0900

    가변인자

---
+++ java8/src/day200328/ComputerEx.java
@@ -0,0 +1,24 @@
+package day200328;
+
+public class ComputerEx {
+	public static void main(String[] args) {
+		Computer com = new Computer();
+		int[] arr = { 10, 20, 30 };
+		int res = com.sum(arr);
+		System.out.println(res);
+
+		int res2 = com.sum(new int[] { 10, 20, 30, 40, 50 });
+		System.out.println(res2);
+
+		int res3 = com.sum2(arr);
+		System.out.println(res3);
+		
+		int res4 = com.sum2(new int[] { 10, 20, 30, 40, 50 });
+		System.out.println(res4);
+		
+		int res5 = com.sum2(10, 20, 30, 40, 50);
+		System.out.println(res5);
+		
+//		int res6 = com.sum(10, 20, 30); -> 사용 못함
+	}
+}
