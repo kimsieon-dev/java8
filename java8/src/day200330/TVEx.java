0e8e19b41163e6d9df826da676ded6766a1121a6
KimSiEon <kimsieon.dev@gmail.com>
2020-03-30 15:57:35 +0900

    연습문제

---
+++ java8/src/day200330/TVEx.java
@@ -0,0 +1,13 @@
+package day200330;
+
+public class TVEx {
+	public static void main(String[] args) {
+
+		TV myTV = new TV("LG", 2017, 32);
+		myTV.show();
+
+	}
+}
+/*
+ * LG에서 만든 2017년형 32인치 TV
+ */
