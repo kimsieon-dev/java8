7b9093f59d396052ee58229b3c8beb7fcb95ade6
KimSiEon <kimsieon.dev@gmail.com>
2020-03-30 13:53:32 +0900

    static

---
+++ java8/src/day200328/CalcStatic.java
@@ -0,0 +1,20 @@
+package day200328;
+
+public class CalcStatic {
+	
+	/* 
+	 * static
+	 * 클래스에 소속
+	 */
+	
+	static double pi = 3.14159;
+	static int number = 10;
+
+	static int plus(int x, int y) {
+		return x + y;
+	}
+	
+	static int minus(int x, int y) {
+		return x - y;
+	}
+}
---
+++ java8/src/day200328/StaticBlock.java
@@ -0,0 +1,13 @@
+package day200328;
+
+public class StaticBlock {
+	static String company = "삼성";
+	static String kind = "LCD";
+	static String product = company + " - " + kind; // 버전 업그레이드 후 가능
+	static String pro2;
+	
+	// java8 버전일때
+	static {
+		pro2 = company + " - " + kind;
+	}
+}
