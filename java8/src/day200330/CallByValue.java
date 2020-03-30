5ddb1059d3fe033cb076bfd0f92051cc7d22f86a
KimSiEon <kimsieon.dev@gmail.com>
2020-03-30 14:24:21 +0900

    call by value

---
+++ java8/src/day200330/CallByValue.java
@@ -0,0 +1,18 @@
+package day200330;
+
+public class CallByValue {
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 20;
+		CallByValue cv = new CallByValue();
+		int res = cv.plus(num1, num2);
+		System.out.println(res);
+		System.out.println(num1 + num2);
+	}
+	
+	int plus(int x, int y) {
+		x += 10;
+		y += 10;
+		return x + y;
+	}
+}
