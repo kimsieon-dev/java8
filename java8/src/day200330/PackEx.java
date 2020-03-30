cc2b7d7f28f3f6c455fb364ab03d3276b1a900f4
KimSiEon <kimsieon.dev@gmail.com>
2020-03-30 14:55:09 +0900

    패키지

---
+++ java8/src/day200330/PackEx.java
@@ -0,0 +1,16 @@
+package day200330;
+
+import java.util.*; // * -> java.util의 모든 것 사용
+
+public class PackEx {
+	public static void main(String[] args) {
+		/*
+		 * java.util.Random rd = new java.util.Random(); 
+		 * 원래는 java.util.Random인데 길어서
+		 * import를 이용해서 java.util을 생략 
+		 */
+		
+		Random rd = new Random();
+		
+	}
+}
