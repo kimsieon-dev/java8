e3121922faa2e275eaaba28fcd3bd7d6974338be
KimSiEon <kimsieon.dev@gmail.com>
2020-03-31 11:32:29 +0900

    StringToken

---
+++ java8/src/day200331/TokenEx.java
@@ -0,0 +1,24 @@
+package day200331;
+
+import java.util.Calendar;
+import java.util.StringTokenizer;
+
+public class TokenEx {
+	public static void main(String[] args) {
+		String now = "2020/03/31";
+		StringTokenizer st = new StringTokenizer(now, "/");
+
+		int cnt = st.countTokens();
+		System.out.println("현재 토큰 수 : " + cnt);
+
+		while (st.hasMoreTokens()) {
+			String temp = st.nextToken();
+			System.out.println(temp);
+		}
+
+		String[] arr = now.split("/");
+		for(String str : arr) {
+			System.out.println(str);
+		}
+	}
+}
