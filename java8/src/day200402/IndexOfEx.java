4dd3821eda9173dbb17e88647027f2ae819048a3
KimSiEon <kimsieon.dev@gmail.com>
2020-04-02 11:34:29 +0900

    indexOf, replace

--- java8/src/day200402/IndexOfEx.java
+++ java8/src/day200402/IndexOfEx.java
@@ -2,16 +2,17 @@ package day200402;
 
 public class IndexOfEx {
 	public static void main(String[] args) {
+		// indexOf
 		String str = "자바 프로그래밍";
 		int index = str.indexOf("프로");
-		
+
 		System.out.println(index);
 		System.out.println();
-		
+
 		index = str.indexOf("C언어");
 		System.out.println(index); // 없을 경우 -1을 리턴
 		System.out.println();
-		
+
 		str = "자바자바자바 프로프로 그래밍이이입니다.";
 		index = str.indexOf("바"); // 매개변수가 한개일때는 0번부터 검색
 		System.out.println(index);
@@ -20,5 +21,10 @@ public class IndexOfEx {
 		index = str.indexOf("바", index + 1);
 		System.out.println(index);
 		index = str.indexOf("바", index + 1);
+
+		// replace
+		String str2 = str.replace("프로프로", "프로");
+		System.out.println(str);
+		System.out.println(str2);
 	}
 }
