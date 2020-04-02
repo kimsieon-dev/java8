a61c3ed9f26a6be0ae9b6404ae97d7e3576e497d
KimSiEon <kimsieon.dev@gmail.com>
2020-04-02 11:23:06 +0900

    CharAt

--- java8/src/day200402/ChatAtEx.java
+++ java8/src/day200402/ChatAtEx.java
@@ -4,10 +4,10 @@ public class ChatAtEx {
 	public static void main(String[] args) {
 		String str = "Hello, World!!!";
 		char ch = str.charAt(4);
-		
+
 		System.out.println(ch);
-		for(int i = 0; i < str.length(); i++) {
-			System.out.print(str.charAt(i));
+		for (int i = 0; i < str.length(); i++) {
+			System.out.println(str.charAt(i));
 		}
 	}
 }
