a403ef1ae049a6d4a5cc64b63f9a6abf78553254
KimSiEon <kimsieon.dev@gmail.com>
2020-04-03 09:50:43 +0900

    챕터05 실습문제01

--- java8/src/day200403/Circle.java
+++ java8/src/day200403/Circle.java
@@ -3,11 +3,15 @@ package day200403;
 public class Circle {
 	static final double PI = 3.14; // 상수 선언
 	int r;
-	
-	double area(int r) {
+
+	public Circle(int r) {
+
+	}
+
+	double area() {
 		return PI * r * r;
 	}
-	
+
 	String showCircle() {
 		return "Radius : " + r;
 	}
