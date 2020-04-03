088758944931b865ef88f07061405a821bbb7280
KimSiEon <kimsieon.dev@gmail.com>
2020-04-03 14:14:55 +0900

    제한된 타입

---
+++ java8/src/day200403/BoundedTypeParameterEx.java
@@ -0,0 +1,15 @@
+package day200403;
+
+public class BoundedTypeParameterEx {
+	public static void main(String[] args) {
+		// String str = Util.compare("a", "b"); -> (x)
+		
+		int result1 = Util.compare(10, 20);
+		System.out.println(result1);
+		
+		int result2 = Util.compare(4.5, 3);
+		System.out.println(result2);
+		
+		
+	}
+}
