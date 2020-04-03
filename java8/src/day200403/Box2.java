661d03adaa211c4998108b57fa12d557fda56142
KimSiEon <kimsieon.dev@gmail.com>
2020-04-03 13:18:13 +0900

    class에서 사용하는 제네릭

---
+++ java8/src/day200403/Box2.java
@@ -0,0 +1,12 @@
+package day200403;
+
+public class Box2<T> { // 제네릭은 타입매개변수
+	private T object;
+	public void set(T object) {
+		this.object = object;
+	}
+	
+	public T get() {
+		return object;
+	}
+}
