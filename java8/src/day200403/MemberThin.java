1960ee5a9695de3fe751e6674af5f48e5fef3653
KimSiEon <kimsieon.dev@gmail.com>
2020-04-03 16:08:46 +0900

    얕은 복제(thin clone)

---
+++ java8/src/day200403/MemberThin.java
@@ -0,0 +1,27 @@
+package day200403;
+
+public class MemberThin {
+	public String id;
+	public String name;
+	public String password;
+	public int age;
+	public boolean adult;
+
+	public MemberThin(String id, String name, String password, int age, boolean adult) {
+		this.id = id;
+		this.name = name;
+		this.password = password;
+		this.age = age;
+		this.adult = adult;
+	}
+
+	public MemberThin getMember() {
+		MemberThin cloned = null;
+		try {
+			cloned = (MemberThin) clone(); // clone() 메소드의 리턴 타입은 Object라서 캐스팅 함
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		return cloned;
+	}
+}
