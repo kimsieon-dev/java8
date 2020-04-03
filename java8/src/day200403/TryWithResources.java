aa53a3094d3d08b52fa6e9620e7f53a9fd1f7148
KimSiEon <kimsieon.dev@gmail.com>
2020-04-03 11:10:25 +0900

    try-with-resources (finally 이용)

---
+++ java8/src/day200403/TryWithResources.java
@@ -0,0 +1,30 @@
+package day200403;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class TryWithResources {
+	public void getData(String file) {
+		Scanner sc = null;
+
+		try {
+			sc = new Scanner(new File(file));
+			System.out.println(sc.nextLine());
+		} catch (Exception e) {
+			System.err.println("예외발생");
+			System.out.println();
+			e.getMessage();
+			System.out.println();
+			e.printStackTrace();
+		} finally { // 입출력 스트림을 강제로 닫기 위해서 finally 구간을 이용(1.6버전 이전)
+			if (sc != null) {
+				sc.close();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		TryWithResources twr = new TryWithResources();
+		twr.getData("test.txt");
+	}
+}
