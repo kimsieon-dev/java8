0e5c6770fb1445baa386c73559362190269d3e80
KimSiEon <kimsieon.dev@gmail.com>
2020-04-06 10:53:56 +0900

    HashSet

--- java8/src/day200406/HashSetEx.java
+++ java8/src/day200406/HashSetEx.java
@@ -1,6 +1,7 @@
 package day200406;
 
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -44,5 +45,7 @@ public class HashSetEx {
 			String str = iterator.next();
 			System.out.println(str);
 		}
+		System.out.println();
+
 	}
 }
