d7dedd7efc0413a8526978293ad41a8b1a85b014
KimSiEon <kimsieon.dev@gmail.com>
2020-04-07 14:14:50 +0900

    람다식 - 로컬 변수

---
+++ java8/src/day200407/UsingLocalVariable.java
@@ -0,0 +1,24 @@
+package day200407;
+
+public class UsingLocalVariable {
+	void method(int arg) { // arg는 final 특성을 가짐
+		int localVar = 40; // localVar는 final 특성을 가짐
+		
+		// arg = 31; // final 특성 때문에 수정 불가
+		// localVar = 41; // final 특성 때문에 수정 불가
+		
+		// 람다식
+		Functional fi = () -> {
+			// 로컬변수 사용
+			System.out.println("arg : " + arg);
+			System.out.println("localVar : " + localVar + "\n");
+		};
+		fi.method();
+	}
+	
+	void method2(int x) {
+		System.out.println(x);
+		x = 50;
+		System.out.println(x);
+	}
+}
---
+++ java8/src/day200407/UsingLocalVariableEx.java
@@ -0,0 +1,8 @@
+package day200407;
+
+public class UsingLocalVariableEx {
+	public static void main(String[] args) {
+		UsingLocalVariable ulv = new UsingLocalVariable();
+		ulv.method(10);
+	}
+}
