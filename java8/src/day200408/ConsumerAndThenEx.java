024da67934c8f64cf9f0847be72610c697ef780c
KimSiEon <kimsieon.dev@gmail.com>
2020-04-08 13:01:37 +0900

    andThen()과 compose() 디폴트 메소드

---
+++ java8/src/day200408/ConsumerAndThenEx.java
@@ -0,0 +1,15 @@
+package day200408;
+
+import java.util.function.Consumer;
+
+public class ConsumerAndThenEx {
+	public static void main(String[] args) {
+		Consumer<Member> consumerA = m -> System.out.println("consumerA : " + m.getName());
+
+		Consumer<Member> consumerB = m -> System.out.println("consumerB : " + m.getId());
+
+		Consumer<Member> consumerAB = consumerA.andThen(consumerB);
+		consumerAB.accept(new Member("ȫ�浿", "hong", null));
+
+	}
+}
