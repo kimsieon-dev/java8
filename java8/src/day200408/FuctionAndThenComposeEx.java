fb57843895afe7eb9ed137aeb5ab3624b1892c7d
KimSiEon <kimsieon.dev@gmail.com>
2020-04-08 13:13:36 +0900

    andThen()과 compose() 디폴트 메소드

--- java8/src/day200408/FuctionAndThenComposeEx.java
+++ java8/src/day200408/FuctionAndThenComposeEx.java
@@ -12,11 +12,11 @@ public class FuctionAndThenComposeEx {
 		functionA = (m) -> m.getAddress();
 		functionB = (a) -> a.getCity();
 
-		functionAB = functionA.andThen(functionB);
+		functionAB = functionA.andThen(functionB); // 매개변수가 2개일때도 andThen 사용 가능
 		city = functionAB.apply(new Member("홍길동", "hong", new Address("한국", "서울")));
 		System.out.println("거주 도시 : " + city);
 
-		functionAB = functionB.compose(functionA);
+		functionAB = functionB.compose(functionA); // 매개변수가 1개일때 compose 사용 가능
 		city = functionAB.apply(new Member("홍길동", "hong", new Address("한국", "서울")));
 		System.out.println("거주 도시 : " + city);
 	}
