65c99ea4572e5ee7afaa8bfe8ee7be997dd66fc0
KimSiEon <kimsieon.dev@gmail.com>
2020-04-09 13:37:27 +0900

    스레드 그룹의 일괄 interrput()

---
+++ java8/src/day200409/WorkThread.java
@@ -0,0 +1,20 @@
+package day200409;
+
+public class WorkThread extends Thread {
+	public WorkThread(ThreadGroup threadGroup, String threadName) {
+		super(threadGroup, threadName);
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				System.out.println(getName() + " interrupted");
+				break;
+			}
+		}
+		System.out.println(getName() + " Á¾·áµÊ");
+	}
+}
