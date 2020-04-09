fb39f76317103927c454fa856e6d8e3d336bb5bf
KimSiEon <kimsieon.dev@gmail.com>
2020-04-09 14:26:14 +0900

    애플릿 색상 설정

---
+++ java8/src/day200409/ColorJApplet.java
@@ -0,0 +1,29 @@
+package day200409;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class ColorJApplet extends JPanel {
+	public void init() {
+		setBackground(Color.orange);
+		/*
+		 * 방법1 
+		 * Panel p1 = new panel(); 
+		 * p1.setBackground(Color.orange); 
+		 * this.add(p1);
+		 */
+	}
+
+	public void paint(Graphics g) {
+		/*
+		 * 방법2 
+		 * paint 메소드에서 fillRect 사용 
+		 * g.setColor(Color.orange); g.fillRect(0, 0, getWidth(), getHeight());
+		 */
+		Color myColor = new Color(255, 0, 0);
+		g.setColor(myColor);
+		g.drawString("검정색 글씨를 그린다.", 40, 100);
+	}
+}
