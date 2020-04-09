8422be6ea197bef7757c00b83226ed4854a60ff4
KimSiEon <kimsieon.dev@gmail.com>
2020-04-09 09:18:25 +0900

    스레드 우선순위

--- java8/src/day200409/PriorityEx.java
+++ java8/src/day200409/PriorityEx.java
@@ -5,9 +5,9 @@ public class PriorityEx {
 		for (int i = 1; i <= 10; i++) {
 			Thread thread = new CalcThread("thread" + i);
 			if (i != 10) {
-				thread.setPriority(Thread.MIN_PRIORITY);
+				thread.setPriority(Thread.MIN_PRIORITY); // 최소 우선순위
 			} else {
-				thread.setPriority(Thread.MAX_PRIORITY);
+				thread.setPriority(Thread.MAX_PRIORITY); // 최대 우선순위
 			}
 			thread.start();
 		}
