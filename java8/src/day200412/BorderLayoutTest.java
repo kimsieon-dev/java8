b52150cd005d09d53c3582c190c887ef12c57798
KimSiEon <kimsieon.dev@gmail.com>
2020-04-13 09:17:04 +0900

    BorderLayout

---
+++ java8/src/day200412/BorderLayoutTest.java
@@ -0,0 +1,33 @@
+package day200412;
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+
+import javax.swing.JFrame;
+
+public class BorderLayoutTest extends JFrame {
+	private static final String EAST = null;
+
+	public BorderLayoutTest() {
+		super("보더 레이아웃");
+		/*
+		 * JFrame과 JApplet의 기본 레이아웃 매니저는 BorderLayout이기 때문에
+		 * BorderLayout 매니저를 생성하지 않아도 된다.
+		 * 각 컴포넌트를 컨테이너에 추가
+		 */
+		
+		add("North", new Button("North"));
+		add(BorderLayout.EAST, new Button("East"));
+		add(new Button("Center"), BorderLayout.CENTER);
+		add(new Button("South"), BorderLayout.SOUTH);
+		add(new Button("West"), BorderLayout.WEST);
+		
+		setSize(300, 200);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		BorderLayoutTest blt = new BorderLayoutTest();
+		blt.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+}
