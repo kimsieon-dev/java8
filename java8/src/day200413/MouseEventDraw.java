5b96db82d459d89fdeea8e6655b345e16130d151
KimSiEon <kimsieon.dev@gmail.com>
2020-04-13 14:12:19 +0900

    MouseEvent

---
+++ java8/src/day200413/MouseEventDraw.java
@@ -0,0 +1,55 @@
+package day200413;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JPanel;
+
+public class MouseEventDraw extends JPanel implements MouseListener {
+	int noval = 0;
+	int[] x, y;
+
+	public MouseEventDraw() {
+		x = new int[50];
+		y = new int[50];
+		addMouseListener(this);
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		g.setColor(Color.blue);
+		g.drawString("마우스를 클릭하면 사각형을 그림", 10, 10);
+		g.setColor(Color.red);
+		for (int i = 0; i < noval; i++) {
+			g.drawRect(x[i] - 20, y[i] - 20, 50, 50);
+		}
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		if (noval < 50) {
+			x[noval] = e.getX();
+			y[noval] = e.getY();
+			noval++;
+		}
+		repaint();
+	}
+
+	public void mouseEntered(MouseEvent e) {
+
+	}
+
+	public void mouseExited(MouseEvent e) {
+
+	}
+
+	public void mousePressed(MouseEvent e) {
+
+	}
+
+	public void mouseReleased(MouseEvent e) {
+
+	}
+
+}
---
+++ java8/src/day200413/MouseEventDrawEx.java
@@ -0,0 +1,15 @@
+package day200413;
+
+import javax.swing.JFrame;
+
+public class MouseEventDrawEx {
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		MouseEventDraw med = new MouseEventDraw();
+
+		f.add(med);
+		f.setSize(250, 250);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
