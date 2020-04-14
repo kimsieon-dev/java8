4b50f6b6e9d4ff54c401d7e84c3f3e8dc2a0c9f5
KimSiEon <kimsieon.dev@gmail.com>
2020-04-14 15:57:32 +0900

    File 클래스

--- java8/src/day200414/FileDirOutput.java
+++ java8/src/day200414/FileDirOutput.java
@@ -10,7 +10,13 @@ public class FileDirOutput {
 			System.out.println("main() 매개변수를 입력하세요");
 			System.exit(1);
 		}
-		dir = args[0];
+		dir = "c:/java";
+		/*
+		 * 경로는 /로 구문(리눅스) \로 구분(윈도우) ,
+		 *  윈도우도 /로 구분 가능. 
+		 * 윈도우에서 \는 특수문자 입력이므로 \\처럼 두개를 입력해야 하나로 인식
+		 */
+		
 		File myFile = new File(dir);
 		String[] flist = myFile.list();
 		System.out.println("검색 디렉토리 : " + dir);
