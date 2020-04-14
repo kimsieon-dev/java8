35bdddfa1b18cc936ab63e614b7339344bfb1e38
KimSiEon <kimsieon.dev@gmail.com>
2020-04-14 14:22:02 +0900

    스트림

---
+++ java8/src/day200414/IStreamReader.java
@@ -0,0 +1,27 @@
+package day200414;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public class IStreamReader {
+	static String fname;
+
+	public static void main(String[] args) {
+		int idata = -1;
+		Reader isr = new InputStreamReader(System.in);
+		System.out.println("키보드에서 문자입력[마지막에 엔터]");
+		System.out.println("=========================");
+
+		try {
+			while (true) {
+				idata = isr.read();
+				if (idata == -1)
+					break;
+				System.out.println((char) idata);
+			}
+		} catch (IOException e) {
+			System.out.println(e.toString());
+		}
+	}
+}
