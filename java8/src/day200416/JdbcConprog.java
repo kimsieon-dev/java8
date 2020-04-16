66117409671cefc3587db3243cb91433c35435bb
KimSiEon <kimsieon.dev@gmail.com>
2020-04-16 13:15:57 +0900

    데이터베이스 연동

---
+++ java8/src/day200416/JdbcConprog.java
@@ -0,0 +1,24 @@
+package day200416;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcConprog {
+	public static void main(String[] args) {
+		String jdbc_url = "jdbc:mysql://localhost:3306/my_database?useUnicode=true"
+				+ "&characterEncoding=UTF8&serverTimezone=UTC";
+		Connection conn = null;
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(jdbc_url, "root", "yt1234");
+			System.out.println("데이터베이스에 접속했습니다.");
+			conn.close();
+		} catch (ClassNotFoundException e) {
+			System.out.println("ClassNotFoundException : " + e.getMessage());
+		} catch (SQLException se) {
+			System.out.println(se.getMessage());
+		}
+	}
+}
